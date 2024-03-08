35cb14a22ce70e72ec03a91fa674728be83eaf2d
carlos <ccarbonell@gmail.com>
2024-03-08 13:24:35 +0100

    added info

--- README.md
+++ README.md
@@ -4,3 +4,6 @@ simple live example of people segmentation
 # install
 wget https://github.com/ayoolaolafenwa/PixelLib/releases/download/0.2.0/pointrend_resnet50.pkl
 pip install -r requirements.txt
+
+# notes
+Play with confidence var to define the margin of complexity
--- main.py
+++ main.py
@@ -7,5 +7,5 @@ capture = cv2.VideoCapture(0)
 segment_video = instanceSegmentation()
 target_classes = segment_video.select_target_classes(person = True)
 
-segment_video.load_model("pointrend_resnet50.pkl", confidence=0.7, detection_speed="rapid")
-segment_video.process_camera(capture, show_bboxes=False, segment_target_classes = target_classes,frames_per_second=15, check_fps=True, show_frames=True,frame_name="frame",output_video_name="output.mp4")
+segment_video.load_model("pointrend_resnet50.pkl", confidence=0.96, detection_speed="rapid")
+segment_video.process_camera(capture, show_bboxes=True, segment_target_classes = target_classes,frames_per_second=15, check_fps=True, show_frames=True,frame_name="frame",output_video_name="output.mp4")
